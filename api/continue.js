0f5741c2f1cadd82dc121447cd0ffcc3c9854c06
Josh Cunningham <josh@joshcanhelp.com>
2020-04-30 17:56:41 -0700

    Cleanup and notes

--- api/continue.js
+++ api/continue.js
@@ -12,15 +12,16 @@ module.exports = async (req, res) => {
 
   const { actionLog, pipeline } = execution;
 
-
   const currentActionLogEntry = actionLog[actionLog.length - 1];
 
-  if(currentActionLogEntry.action !== 'continue') {
+  // TODO: What would be reasons for the server calling this? Can we recover?
+  if (currentActionLogEntry.action !== 'continue') {
     return res.status(400).json({ error: 'continue_unavailable' });
   }
 
   currentActionLogEntry.status = 'success';
 
+  // TODO: Could we get into a situation where we have unfinished actions out of order?
   // find any action in the pipeline that doesn't have a log entry
   const remainingActions = pipeline.filter(action => {
     const matchingLog = actionLog.find(actionLogEntry => actionLogEntry.name === action.name);
@@ -33,13 +34,26 @@ module.exports = async (req, res) => {
     remainingActions
   );
 
+  // TOOD: Still a lot of shared logic with execute.
+  // Maybe /execute takes an execution ID and remove /continue?
   const response = {
     executionId,
     status: newActionLog[newActionLog.length - 1].status,
     actionLog: newActionLog
   };
 
-  store.set(executionId, {user: newUser, context: newContext, config: newConfig, actionLog: newActionLog, pipeline: execution.pipeline });
+  store.set(
+    executionId,
+    {
+      user: newUser,
+      context: newContext,
+      config: newConfig,
+      actionLog: newActionLog,
+      pipeline: execution.pipeline
+    }
+  );
+
+  // TODO: Maybe when actions are complete, store execution in logs and rm from memory?
 
   return res.json(response);
 };
